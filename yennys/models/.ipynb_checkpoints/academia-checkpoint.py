f5b526995daffad331bc239f7763556c172b7dfe
yenny <yg@todoo.co>
2020-02-24 23:15:30 +0000

    ADD 24-2

--- yennys/models/.ipynb_checkpoints/academia-checkpoint.py
+++ yennys/models/.ipynb_checkpoints/academia-checkpoint.py
@@ -1,18 +1,18 @@
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 
 from odoo import models, fields
 
 
-class hola(models.Model):
-     _name = 'hola.usuarios'
-     _description = 'esta clase almacena los usuarios'
+class academia(models.Model):
+     _name = 'academia.usuarios'
+     _description = 'esta clase clase almacena los usuarios'
 
-     name = fields.Char()
+     nombre = fields.Char()
      apellido = fields.Char()
      cedula = fields.Integer()
      matricula = fields.Float(compute="_value_pc", store=True)
      memo = fields.Text()
-
+#
 #     @api.depends('value')
 #     def _value_pc(self):
 #         for record in self:

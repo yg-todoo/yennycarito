ab349a9e34e11fe227a93523363694a914966eef
Yenny <yg@todoo.co>
2020-02-24 23:27:58 +0000

    ADD 24-2

--- yennys/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ yennys/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -24,7 +24,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/vista_academia.xml',
+        #'views/vista_academia.xml',
         #'views/templates.xml',
     ],
     # only loaded in demonstration mode

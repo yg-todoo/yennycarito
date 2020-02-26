bf16918ba169fdae7db525a3e8d16eff2d5e9de4
yg-todoo <60189034+yg-todoo@users.noreply.github.com>
2020-02-26 18:05:25 -0500

    AHORASI

---
+++ my_module/my_module/models/models.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class my_module(models.Model):
+     _name = 'my_module.my_module'
+
+     name = fields.Char()
+     value = fields.Integer()
+     value2 = fields.Float(compute="_value_pc", store=True)
+     description = fields.Text()
+     HV=fields.Binary()
+     marcas=fields.Char()
+
+     @api.depends('value')
+     def _value_pc(self):
+         self.value2 = float(self.value) / 100
